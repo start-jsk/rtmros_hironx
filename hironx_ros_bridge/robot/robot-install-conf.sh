139cb0aa1ebd03227e572fe2e6094244cf20ea90
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-09-05 04:06:13 +0000

    add robot-install-conf.sh

---
+++ hironx_ros_bridge/robot/robot-install-conf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function usage {
+    echo >&2 "usage: $0 [hostname (default:hiro014)]"
+    echo >&2 "          [-h|--help] print this message"
+    exit 0
+}
+
+# command line parse
+OPT=`getopt -o h -l help -- $*`
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- $OPT
+
+while [ -n "$1" ] ; do
+    case $1 in
+        -h|--help) usage ;;
+        --) shift; break;;
+        *) echo "Unknown option($1)"; usage;;
+    esac
+done
+
+address=`host rtm-ros-robotics.googlecode.com | awk '/^[[:alnum:].-]+ has address/ { print $4 ; exit }'`
+
+commands="
+  echo \"* Download hironx_ros_bridge/robot *\";
+  mkdir -p src
+  cd src
+  svn co https://rtm-ros-robotics.googlecode.com/svn/trunk/rtmros_hironx/hironx_ros_bridge/robot robot
+  echo \"* Configure configure files *\";
+  cd robot;
+  PATH=/opt/jsk/bin:$PATH make
+  echo \"* Install configure files *\";
+  PATH=/opt/jsk/bin:$PATH su -c 'make install'
+  "
+hostname=$1
+hostname=${hostname:="hiro014"} 
+read -p "execute install command @ $hostname (y/n)?"
+if [ "$REPLY" == "y" ]; then
+    ssh root@$hostname -t $commands
+else
+    echo "DO NOT RUN"
+    echo "----"
+    echo "$commands"
+    echo "----"
+    echo "EXITTING.."
+fi
+
