663174456f44bcd66d8f7122166b3a0564b6e96b
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-09-11 01:23:09 +0000

    add jenkins-rosbuild.sh

---
+++ jenkins-rosbuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+trap 'exit 1' ERR
+
+function setup {
+    rm -fr rosbuild_ws
+    mkdir -p rosbuild_ws
+    cd rosbuild_ws
+    unset ROS_PACKAGE_PATH
+    rosws init
+    rosws merge https://rtm-ros-robotics.googlecode.com/svn/trunk/rtm-ros-robotics.rosinstall
+    rosws update
+    cd ..
+}
+
+source /opt/ros/groovy/setup.bash
+setup
+cd rosbuild_ws
+source setup.bash
+rosmake hironx_ros_bridge
+
+source `rospack find openrtm_tools`/scripts/rtshell-setup.sh
+rtmtest hironx_ros_bridge hironx-test.launch
+rtmtest hironx_ros_bridge hironx-ros-bridge-test.launch
