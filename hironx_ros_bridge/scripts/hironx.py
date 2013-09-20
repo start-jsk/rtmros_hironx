e8fa6fa6426b603f4f86d7eca54dcd2647d51c34
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-09-20 04:40:17 +0000

    fix command line arguments dicussed in Issue 195

--- hironx_ros_bridge/scripts/hironx.py
+++ hironx_ros_bridge/scripts/hironx.py
@@ -326,18 +326,31 @@ class HIRONX(HrpsysConfigurator):
         print 'Turn on Hand Servo'
         self.sc_svc.servoOn()
 
+import argparse
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='hiro command line interpreters')
+    parser.add_argument('--host', help='corba name server hostname')
+    parser.add_argument('--port', help='corba name server port number')
+    parser.add_argument('--modelfile', help='robot model file nmae')
+    parser.add_argument('--robot', help='robot modlule name (RobotHardware0 for real robot, Robot()')
+    args = parser.parse_args()
+
+    if args.host:
+        rtm.nshost = args.host
+    if args.port:
+        rtm.nsport = args.port
+    if not args.robot:
+        args.robot = "RobotHardware0" if args.host else "HiroNX(Robot)0"
+    if not args.modelfile:
+        args.modelfile = ""
     hiro = HIRONX()
-    if len(sys.argv) > 2:
-        hiro.init(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) > 1:
-        hiro.init(sys.argv[1])
-    else:
-        hiro.init()
+    hiro.init(robotname=args.robot,url=args.modelfile)
 
 # for simulated robot
 # $ ./hironx.py
 #
 # for real robot
-# ./hironx.py  RobotHardware0  /opt/jsk/etc/HIRONX/model/main.wrl -- -ORBInitRef NameService=corbaloc:iiop:hiro014:15005/NameService
-# ipython -i hironx.py  RobotHardware0  /opt/jsk/etc/HIRONX/model/main.wrl -- -ORBInitRef NameService=corbaloc:iiop:hiro014:15005/NameService
+# ./hironx.py  --host hiro014
+# ./ipython -i hironx.py --host hiro014
+# for real robot with custom moelf ile
+# ./hironx.py  --host hiro014 --modelfile /opt/jsk/etc/HIRONX/model/main.wrl
