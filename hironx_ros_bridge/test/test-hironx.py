62b59f63d17db6e8c57ba08454e1a86841b6c93d
kei.okada <kei.okada@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-11-29 12:13:37 +0000

    split test-hironx-ik from test-hironx.py

--- hironx_ros_bridge/test/test-hironx.py
+++ hironx_ros_bridge/test/test-hironx.py
@@ -23,7 +23,7 @@ import numpy as np
 
 from rtm import connectPorts, disconnectPorts
 
-class TestHiroNX(unittest.TestCase):
+class TestHiro(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -33,67 +33,6 @@ class TestHiroNX(unittest.TestCase):
     def test_goInitial(self):
         self.limbbody_init()
         self.robot.goInitial()
-
-    def angle_vector_generator(self):
-        step = 80
-        for i0 in range(-80,80,step):
-            for i1 in range(-130,50,step):
-                for i2 in range(-150,10,step):
-                    for i3 in range(-160,100,step):
-                        for i4 in range(-80,80,step):
-                            for i5 in range(-160,160,step):
-                                yield [i0, i1, i2, i3, i4, i5]
-
-    def test_ik_joint_angle(self):
-        lav = self.angle_vector_generator().next()
-        for av in self.angle_vector_generator():
-            print "av", av
-            self.robot.setJointAnglesOfGroup("LARM", av, 2)
-            self.robot.waitInterpolationOfGroup("LARM")
-            pos1 = self.robot.getReferencePosition("LARM_JOINT5")
-            rpy1 = self.robot.getReferenceRPY("LARM_JOINT5")
-            if numpy.linalg.norm(numpy.array(lav) - numpy.array(av)) > 10:
-                lav = av
-            self.robot.setJointAnglesOfGroup("LARM", lav, 2)
-            self.robot.waitInterpolationOfGroup("LARM")
-            self.assertTrue(self.robot.setTargetPose("LARM", pos1, rpy1, 5))
-            self.robot.waitInterpolationOfGroup("LARM")
-            pos2 = self.robot.getReferencePosition("LARM_JOINT5")
-            rpy2 = self.robot.getReferenceRPY("LARM_JOINT5")
-            print "pos", pos1, pos2, numpy.linalg.norm(numpy.array(pos1)-numpy.array(pos2))
-            print "rpy", rpy1, rpy2, numpy.linalg.norm(numpy.array(rpy1)-numpy.array(rpy2))
-            self.assertTrue(numpy.linalg.norm(numpy.array(pos1)-numpy.array(pos2))<5.0e-3) # 0.005 m
-            self.assertTrue(numpy.linalg.norm(numpy.array(rpy1)-numpy.array(rpy2))<5.0e-3) # 0.286 deg
-            lav = av
-
-    def target_point_generator(self,xmin,xmax,ymin,ymax,zmin,zmax,step):
-        for x in range(xmin,xmax,step):
-            for y in range(ymin,ymax,step):
-                for z in range (zmin,zmax,step):
-                    yield [x/1000.0, y/1000.0, z/1000.0]
-
-    def test_ik_left(self):
-        self.limbbody_init()
-        self.pos_ik_test("LARM", 220,400, -5,320, 5,300, 100) # zmax=390
-
-    def test_ik_right(self):
-        self.limbbody_init()
-        self.pos_ik_test("RARM", 220,400, -320,5,  5,300, 100) # zmax=390
-
-    def pos_ik_test(self, arm, xmin,xmax,ymin,ymax,zmin,zmax,step):
-        arm_target = arm+"_JOINT5"
-        rot1 = [[0,0,-1],[0,1,0],[1,0,0]]
-        for pos1 in self.target_point_generator(xmin,xmax,ymin,ymax,zmin,zmax,step):
-            self.robot.goInitial()
-            print self.robot.getReferenceRPY(arm_target)
-            self.assertTrue(self.robot.setTargetPose(arm, pos1, euler_from_matrix(rot1), 5))
-            self.robot.waitInterpolationOfGroup(arm)
-            pos2 = self.robot.getReferencePosition(arm_target)
-            rot2 = self.robot.getReferenceRotation(arm_target)
-            print "pos", pos1, pos2, numpy.linalg.norm(numpy.array(pos1)-numpy.array(pos2))
-            print "rpy", rot1, rot2, numpy.linalg.norm(numpy.array(rot1)-numpy.array(rot2))
-            self.assertTrue(numpy.linalg.norm(numpy.array(pos1)-numpy.array(pos2))<5.0e-3) # 0.005 m
-            self.assertTrue(numpy.linalg.norm(numpy.array(rot1)-numpy.array(rot2))<5.0e-3) # 0.286 deg
 
     def test_goOffPose(self):
         self.limbbody_init()
@@ -204,7 +143,7 @@ class TestHiroNX(unittest.TestCase):
         data_time = data[-1][0] - data[0][0]
         min_data = min([d[1] for d in data])
         max_data = max([d[1] for d in data])
-        print "check setJointAngles(wait=True),  tm = ", data_time, ", ok?", abs(data_time - 10.0) < 0.1
+        print "check setJointAngles(wait=True),  tm = ", data_time, ", ok?", abs(data_time - 10.0) < 0.1, " ", 10
         print "                                 min = ", min_data, ", ok?", abs(min_data - -140) < 5
         print "                                 max = ", max_data, ", ok?", abs(max_data - -100) < 5
 
@@ -225,7 +164,7 @@ class TestHiroNX(unittest.TestCase):
             data_time = data[-1][0] - data[0][0]
             min_data = min([d[1] for d in data])
             max_data = max([d[1] for d in data])
-            print "check setJointAngles(wait=False), tm = ", data_time, ", ok?", abs(data_time - (10.0 - (5 - clear_time[i]))) < 0.1
+            print "check setJointAngles(wait=False), tm = ", data_time, ", ok?", abs(data_time - (10.0 - (5 - clear_time[i]))) < 0.1, " ", (10.0 - (5 - clear_time[i]))
             print "                                 min = ", min_data, ", ok?", abs(min_data - (-140+i*40/len(clear_time))) < 10, " ", -140+i*40/len(clear_time)
             print "                                 max = ", max_data, ", ok?", abs(max_data - -100) < 5
 
@@ -249,7 +188,7 @@ class TestHiroNX(unittest.TestCase):
             data_time = data[-1][0] - data[0][0]
             min_data = min([d[1] for d in data])
             max_data = max([d[1] for d in data])
-            print "check setJointAngles(clear),      tm = ", data_time, ", ok?", abs(data_time - 5) < 0.1
+            print "check setJointAngles(clear),      tm = ", data_time, ", ok?", abs(data_time - 5) < 0.1, " ", 5
             print "                                 min = ", min_data, ", ok?", abs(min_data - (-140+(i+1)*40/len(clear_time))) < 10, " ", -140+(i+1)*40/len(clear_time)
             print "                                 max = ", max_data, ", ok?", abs(max_data - -100) < 5
 
@@ -283,7 +222,7 @@ class TestHiroNX(unittest.TestCase):
         data_time = data[-1][0] - data[0][0]
         min_data = min([d[1] for d in data])
         max_data = max([d[1] for d in data])
-        print "check setJointAnglesOfGroup(wait=True),  tm = ", data_time, ", ok?", abs(data_time - 15.0) < 0.1
+        print "check setJointAnglesOfGroup(wait=True),  tm = ", data_time, ", ok?", abs(data_time - 15.0) < 0.1, " ", 15
         self.assertTrue(abs(data_time - 15.0) < 0.1)
         print "                                        min = ", min_data, ", ok?", abs(min_data - -140) < 5
         self.assertTrue(abs(min_data - -140) < 5)
@@ -294,9 +233,9 @@ class TestHiroNX(unittest.TestCase):
     def test_rarm_setJointAngles_NoWait (self):
         self.limbbody_init()
         clear_time = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0]
+        self.robot.setJointAnglesOfGroup("rarm", [-0.6, 0, -100, 15.2, 9.4, 3.2], 5, wait=False);
+        self.robot.waitInterpolationOfGroup("rarm")
         for i in range(len(clear_time)):
-            self.robot.setJointAnglesOfGroup("rarm", [-0.6, 0, -120, 15.2, 9.4, 3.2], 5, wait=False);
-            self.robot.waitInterpolationOfGroup("rarm")
             self.robot.clearLog()
 
             self.robot.setJointAnglesOfGroup("rarm", [-0.6, 0, -140, 15.2, 9.4, 3.2], 5, wait=False);
@@ -310,7 +249,7 @@ class TestHiroNX(unittest.TestCase):
             data_time = data[-1][0] - data[0][0]
             min_data = min([d[1] for d in data])
             max_data = max([d[1] for d in data])
-            print "check setJointAnglesOfGroup(wait=False), tm = ", data_time, ", ok?", abs(data_time - (10.0 - (5 - clear_time[i]))) < 0.1
+            print "check setJointAnglesOfGroup(wait=False), tm = ", data_time, ", ok?", abs(data_time - (10.0 - (5 - clear_time[i]))) < 0.1, " ", (10.0 - (5 - clear_time[i]))
             self.assertTrue(abs(data_time - (10.0 - (5 - clear_time[i]))) < 0.1)
             print "                                        min = ", min_data, ", ok?", abs(min_data - (-140+i*40/len(clear_time))) < 20, " ", -140+i*40/len(clear_time)
             self.assertTrue(abs(min_data - (-140+i*40/len(clear_time))) < 20)
@@ -444,7 +383,7 @@ class TestHiroNX(unittest.TestCase):
         os.system(cmd)
 
         # assertion
-        print "check setJointAnglesOfGroup with Accel,  tm = ", data_time, ", ok?", abs(data_time - 9) < 0.5, data_time
+        print "check setJointAnglesOfGroup with Accel,  tm = ", data_time, ", ok?", abs(data_time - 9) < 0.5, 9
         self.assertTrue(abs(data_time - 9) < 0.5)
         print "                                        min = ", min_data, ", ok?", abs(min_data - -135) < 5, " "
         self.assertTrue(abs(min_data - -135) < 5)
@@ -466,8 +405,5 @@ class TestHiroNX(unittest.TestCase):
 # unittest.main()
 if __name__ == '__main__':
     import rostest
-    rostest.rosrun(PKG, 'test_hronx', TestHiroNX) 
-
-
-
+    rostest.rosrun(PKG, 'test_hronx', TestHiro) 
 
