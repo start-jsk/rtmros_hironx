1546a64c64e7a7b683c433cadfdc4ae892b248f3
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-02-19 17:24:14 +0900

    fix robot-compile script

--- hironx_ros_bridge/robot/robot-compile-hrpsys.sh
+++ hironx_ros_bridge/robot/robot-compile-hrpsys.sh
@@ -25,25 +25,38 @@ done
 ## Comment out; not used.
 #address=`host hrpsys-base.googlecode.com | awk '/^[[:alnum:].-]+ has address/ { print $4 ; exit }'` # this does not work for  Server certificate verification 
 
+PID=28169
+
+wget https://raw.github.com/start-jsk/hrpsys/master/package.xml -O /tmp/hrpsys-package.xml
+HRPSYS_VERSION=`sed -n 's@\s*<version>\(.*\)</version>@\1@p' /tmp/hrpsys-package.xml`
+(cd ../; tar -cvzf /tmp/hironx-robot-script-$PID.tgz robot/Makefile robot/*.in robot/*.sav)
+
+
 commands="
   . ~/.profile;
   env;
   echo \"* Download hrpsys *\";
-  mkdir -p src;
-  cd src;
-  svn co http://hrpsys-base.googlecode.com/svn/trunk hrpsys-base-source;
+  mkdir -p /tmp/hrpsys-source-$PID/src;
+  cd /tmp/hrpsys-source-$PID/src;
+  svn co http://hrpsys-base.googlecode.com/svn/tags/${HRPSYS_VERSION} hrpsys-base-source-${HRPSYS_VERSION};
   echo \"* Configure hrpsys *\";
-  rm -fr hrpsys-base-build;
-  mkdir -p hrpsys-base-build;
-  cd hrpsys-base-build;
-  svn info ../hrpsys-base-source > 00ReadMe;
-  PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc TVMET_DIR=/opt/jsk OPENRTM_IDL_DIR=/opt/jsk/include/OpenRTM-1.1/rtm/idl LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem\" cmake ../hrpsys-base-source -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DOPT_COLLADASUPPORT=NO -DEIGEN_INCLUDE_DIR=/opt/jsk/include -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=/opt/jsk -DBLAS_LIBRARY=/opt/jsk/lib/libblas.so -DLAPACK_LIBRARY=/opt/jsk/lib/liblapack.so -DG2C_LIBRARY=/opt/jsk/lib/libf2c.so -DCMAKE_CXX_FLAGS=\"-I/usr/pkg/include\" -DCMAKE_INSTALL_PREFIX=/opt/jsk -DENABLE_DOXYGEN=OFF ;
+  cd hrpsys-base-source-${HRPSYS_VERSION};
+  PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc TVMET_DIR=/opt/jsk OPENRTM_IDL_DIR=/opt/jsk/include/OpenRTM-1.1/rtm/idl LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem\" cmake . -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DOPT_COLLADASUPPORT=NO -DEIGEN_INCLUDE_DIR=/opt/jsk/include -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../ -DBLAS_LIBRARY=/opt/jsk/lib/libblas.so -DLAPACK_LIBRARY=/opt/jsk/lib/liblapack.so -DG2C_LIBRARY=/opt/jsk/lib/libf2c.so -DCMAKE_CXX_FLAGS=\"-I/usr/pkg/include\" -DCMAKE_INSTALL_PREFIX=../../ -DENABLE_DOXYGEN=OFF ;
   echo \"* Compile hrpsys *\";
   PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib:/usr/qnx650/host/qnx6/x86/usr/lib make VERBOSE=1;
-  echo \"* package hrpsys *\";
-  tar -cvzf ../hrpsys-base-\`svn info ../hrpsys-base-source | grep Revision | cut -d ' ' -f 2\`.tgz ./;
-  echo \"* Install hrpsys *\";
-  su -c 'make install; rm /opt/jsk/lib/libhrpIo.so';
+  make install;
+  echo \"* Modify codes *\";
+  cd /tmp;
+  rm hrpsys-source-$PID/lib/libhrpIo.so;
+  (cd /tmp/hrpsys-source-$PID; sed -i s@/tmp/hrpsys-source-$PID@/opt/jsk@g bin/hrpsyspy lib/pkgconfig/hrpsys-base.pc share/hrpsys/samples/*/*.{conf,xml,sh};);
+  echo \"* Configure robot script files  *\";
+  tar -xkvzf /tmp/hironx-robot-script-$PID.tgz;
+  cd robot;
+  make configure;
+  make install INSTALL_DIR=/tmp/hrpsys-source-$PID;
+  cd ..;
+  echo \"* make tarball *\";
+  tar -C hrpsys-source-$PID  -czf hrpsys-qnx-${HRPSYS_VERSION}.tgz ./;
   "
 
 hostname=$1
@@ -53,7 +66,11 @@ userid=${userid:="hiro"}
 echo "comands = $commands"
 read -p "execute compile command @ $hostname (y/n)?"
 if [ "$REPLY" == "y" ]; then
-    ssh $userid@$hostname -t $commands
+    scp /tmp/hironx-robot-script-$PID.tgz $userid@$hostname:/tmp/
+    ssh $userid@$hostname -t $commands 2>&1 | tee /tmp/robot-compile-hrpsys-`date +"%Y%m%d-%H%M%S"`.log
+    echo "====="
+    echo "$ cd /opt/jsk"
+    echo "$ tar -xvzf /tmp/hrpsys-qnx-${HRPSYS_VERSION}.tgz"
 else
     echo "DO NOT RUN"
     echo "----"
---
+++ hironx_ros_bridge/robot/robot-compile-openhrp3.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+function usage {
+    echo >&2 "usage: $0 [hostname (default:hiro014)]"
+    echo >&2 "          [-h|--help] print this message"
+    exit 0
+}
+
+# command line parse
+OPT=`getopt -o h -l help -- $*`
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- $OPT
+
+while [ -n "$1" ] ; do
+    case $1 in
+        -h|--help) usage ;;
+        --) shift; break;;
+        *) echo "Unknown option($1)"; usage;;
+    esac
+done
+
+PID=28169
+
+wget https://raw.github.com/start-jsk/openhrp3/master/package.xml -O /tmp/openhrp3-package.xml
+OPENHRP3_VERSION=`sed -n 's@\s*<version>\(.*\)</version>@\1@p' /tmp/openhrp3-package.xml`
+
+
+commands="
+  . ~/.profile;
+  env;
+  set +x;
+  echo \"* Download openhrp3 *\";
+  mkdir -p /tmp/openhrp3-source-$PID/src;
+  cd /tmp/openhrp3-source-$PID/src;
+  (echo 'p' | svn co https://openrtp.jp/svn/hrg/openhrp/3.1/tags/${OPENHRP3_VERSION} OpenHRP-${OPENHRP3_VERSION}) ||  tar -xzf /tmp/OpenHRP-${OPENHRP3_VERSION}.tgz;
+  echo \"* Configure openhrp3 *\";
+  cd OpenHRP-${OPENHRP3_VERSION};
+  PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem\" cmake . -DEIGEN_INCLUDE_DIR=/opt/jsk/include/eigen3 -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DENABLE_INSTALL_RPATH=ON -DENABLE_INSTALL_RPATH_TO_SELF=ON -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../../;
+  echo \"* Compile openhrp3 *\";
+  PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib:/usr/qnx650/host/qnx6/x86/usr/lib make VERBOSE=1;
+  make install;
+  echo \"* Modify codes *\";
+  cd /tmp;
+  (cd openhrp3-source-$PID; sed -i s@/tmp/openhrp3-source-$PID@/opt/jsk@g lib/pkgconfig/openhrp3.1.pc bin/openhrp-jython-prompt include/OpenHRP-3.1/hrpModel/Config.h;);
+  echo \"* make tarball*\";
+  tar -C openhrp3-source-$PID  -czf openhrp3-qnx-${OPENHRP3_VERSION}.tgz ./;
+  "
+
+hostname=$1
+hostname=${hostname:="hiro014"} 
+userid=$2
+userid=${userid:="hiro"} 
+echo "comands = $commands"
+read -p "execute compile command @ $hostname (y/n)?"
+if [ "$REPLY" == "y" ]; then
+    ssh $userid@$hostname -t $commands 2>&1 | tee /tmp/robot-compile-openhrp3-`date +"%Y%m%d-%H%M%S"`.log
+    echo "====="
+    echo "$ cd /opt/jsk"
+    echo "$ tar -xvzf /tmp/openhrp3-qnx-${OPENHRP3_VERSION}.tgz"
+else
+    echo "DO NOT RUN"
+    echo "----"
+    echo "$commands"
+    echo "----"
+    echo "EXITTING.."
+fi
+
