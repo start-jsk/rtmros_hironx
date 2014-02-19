c8631ce92fdc546b67b7615efb83b4e4066ecb23
leus <parsnip@gmail.com>
2014-02-19 20:29:11 +0900

    add OPENRTM_DIR

--- hironx_ros_bridge/robot/robot-compile-openhrp3.sh
+++ hironx_ros_bridge/robot/robot-compile-openhrp3.sh
@@ -31,6 +31,7 @@ OPENHRP3_VERSION=`sed -n 's@\s*<version>\(.*\)</version>@\1@p' /tmp/openhrp3-pac
 commands="
   . ~/.profile;
   env;
+  trap 'exit 1' ERR;
   set +x;
   echo \"* Download openhrp3 *\";
   mkdir -p /tmp/openhrp3-source-$PID/src;
@@ -38,7 +39,7 @@ commands="
   (echo 'p' | svn co https://openrtp.jp/svn/hrg/openhrp/3.1/tags/${OPENHRP3_VERSION} OpenHRP-${OPENHRP3_VERSION}) ||  tar -xzf /tmp/OpenHRP-${OPENHRP3_VERSION}.tgz;
   echo \"* Configure openhrp3 *\";
   cd OpenHRP-${OPENHRP3_VERSION};
-  PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem\" cmake . -DEIGEN_INCLUDE_DIR=/opt/jsk/include/eigen3 -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DENABLE_INSTALL_RPATH=ON -DENABLE_INSTALL_RPATH_TO_SELF=ON -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../../;
+  PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem\" cmake . -DEIGEN_INCLUDE_DIR=/opt/jsk/include/eigen3 -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DENABLE_INSTALL_RPATH=ON -DENABLE_INSTALL_RPATH_TO_SELF=ON -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../../;
   echo \"* Compile openhrp3 *\";
   PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib:/usr/qnx650/host/qnx6/x86/usr/lib make VERBOSE=1;
   make install;
