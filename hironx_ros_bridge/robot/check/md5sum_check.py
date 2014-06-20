cfee26ef42a6df56ca776ab821b38acff760f44b
lokada <lokada@jenkins.jsk.imi.i.u-tokyo.ac.jp>
2014-06-20 19:26:14 +0900

    fix typoe

--- hironx_ros_bridge/robot/check/md5sum_check.py
+++ hironx_ros_bridge/robot/check/md5sum_check.py
@@ -28,7 +28,7 @@ def md5sum_check_dir(dir_info):
 
   filename = full_dir.replace("/","-") + ".py"
   if filename[0] == "-":
-    filenaem = filename[1:]
+    filename = filename[1:]
   filename = "md5sum-" + filename
 
   print filename
