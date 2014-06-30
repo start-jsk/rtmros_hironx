771fdab87d10dd04b563e40edf961b745a55f1df
lokada <lokada@jenkins.jsk.imi.i.u-tokyo.ac.jp>
2014-06-30 20:59:07 +0900

    update robot-compile-*.sh

--- hironx_ros_bridge/robot/robot-compile-hrpsys.sh
+++ hironx_ros_bridge/robot/robot-compile-hrpsys.sh
@@ -22,60 +22,54 @@ while [ -n "$1" ] ; do
     esac
 done
 
-## Comment out; not used.
-#address=`host hrpsys-base.googlecode.com | awk '/^[[:alnum:].-]+ has address/ { print $4 ; exit }'` # this does not work for  Server certificate verification 
-
-PID=$$
 
 HOSTNAME=${1-"hiro014"}
 USERID=${2-"hiro"}
-HRPSYS_VERSION=${3-""}
+HRPSYS_VERSION=${3-"315.1.10"}
 
-if [[ ! $HRPSYS_VERSION ]]; then
-    wget https://raw.github.com/start-jsk/hrpsys/master/package.xml -O /tmp/hrpsys-package.xml
-    HRPSYS_VERSION=`sed -n 's@\s*<version>\(.*\)</version>@\1@p' /tmp/hrpsys-package.xml`
-    #(cd ../; tar -cvzf /tmp/hironx-robot-script-$PID.tgz robot/Makefile robot/*.in robot/*.sav)
-fi
 
-exit
+DATE=`date +%Y-%m-%d`
+
+#wget https://github.com/fkanehiro/hrpsys-base/archive/${HRPSYS_VERSION}.zip -O /tmp/hrpsys-base-${HRPSYS_VERSION}.zip || echo "ERROR:: Failed to download source code"
+(cd ../; tar -cvzf /tmp/hironx-robot-script-$DATE.tgz robot/Makefile robot/*.in robot/*.sav)
 
 commands="
   . ~/.profile;
   env;
+  trap 'exit 1' ERR;
+  set +x;
   echo \"* Download hrpsys *\";
-  mkdir -p /tmp/hrpsys-source-$PID/src;
-  cd /tmp/hrpsys-source-$PID/src;
-  git clone http://github.com/fkanehiro/hrpsys-base hrpsys-base-source-${HRPSYS_VERSION};
-  cd hrpsys-base-source-${HRPSYS_VERSION};
-  git fetch -q origin '+refs/pull/*:refs/remotes/pull/*'
-  git checkout -qf $TRAVIS_COMMIT
+  mkdir -p /tmp/hrpsys-source-$DATE/src;
+  cd /tmp/hrpsys-source-$DATE/src;
+  mv /tmp/hrpsys-base-$HRPSYS_VERSION.zip .;
+  unzip -o hrpsys-base-$HRPSYS_VERSION.zip ;
+  mkdir -p ../build;
+  cd ../build;
   echo \"* Configure hrpsys *\";
-  PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc TVMET_DIR=/opt/jsk OPENRTM_IDL_DIR=/opt/jsk/include/OpenRTM-1.1/rtm/idl LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem\" cmake . -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DOPT_COLLADASUPPORT=NO -DEIGEN_INCLUDE_DIR=/opt/jsk/include -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../ -DBLAS_LIBRARY=/opt/jsk/lib/libblas.so -DLAPACK_LIBRARY=/opt/jsk/lib/liblapack.so -DG2C_LIBRARY=/opt/jsk/lib/libf2c.so -DCMAKE_CXX_FLAGS=\"-I/usr/pkg/include\" -DCMAKE_INSTALL_PREFIX=../../ -DENABLE_DOXYGEN=OFF ;
+  PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc TVMET_DIR=/opt/jsk OPENRTM_IDL_DIR=/opt/jsk/include/OpenRTM-1.1/rtm/idl LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem\" cmake ../src/hrpsys-base-${HRPSYS_VERSION} -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DOPT_COLLADASUPPORT=NO -DEIGEN_INCLUDE_DIR=/opt/jsk/include -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -luuid -lsocket -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_thread -lboost_filesystem -lboost_regex -lf2c -luuid -lsocket -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_thread -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../ -DBLAS_LIBRARY=/opt/jsk/lib/libblas.so -DLAPACK_LIBRARY=/opt/jsk/lib/liblapack.so -DG2C_LIBRARY=/opt/jsk/lib/libf2c.so -DCMAKE_CXX_FLAGS=\"-I/usr/pkg/include\" -DCMAKE_INSTALL_PREFIX=/opt/jsk -DENABLE_DOXYGEN=OFF ;
   echo \"* Compile hrpsys *\";
   PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib:/usr/qnx650/host/qnx6/x86/usr/lib make VERBOSE=1;
+  echo \"* Install hrpsys *\";
   make install;
   echo \"* Modify codes *\";
-  cd /tmp;
-  rm hrpsys-source-$PID/lib/libhrpIo.so;
-  (cd /tmp/hrpsys-source-$PID; sed -i s@/tmp/hrpsys-source-$PID@/opt/jsk@g bin/hrpsyspy lib/pkgconfig/hrpsys-base.pc share/hrpsys/samples/*/*.{conf,xml,sh};);
+  rm /opt/jsk/lib/libhrpIo.so;
   echo \"* Configure robot script files  *\";
-  tar -xkvzf /tmp/hironx-robot-script-$PID.tgz;
+  tar -xkvzf /tmp/hironx-robot-script-$DATE.tgz;
   cd robot;
   make configure;
-  make install INSTALL_DIR=/tmp/hrpsys-source-$PID;
-  cd ..;
+  make install INSTALL_DIR=/opt/jsk/;
+  cd /tmp;
   echo \"* make tarball *\";
-  tar -C hrpsys-source-$PID  -czf hrpsys-qnx-${HRPSYS_VERSION}.tgz ./;
+  tar -czf hrpsys-${HRPSYS_VERSION}-qnx-${DATE}.tgz ./hrpsys-source-$DATE/;
   "
 
 echo "comands = $commands"
-read -p "execute compile command @ $HOSTNAME (y/n)?"
+read -p "execute compile command @ $HOSTNAME (y/n)? "
 if [ "$REPLY" == "y" ]; then
-    scp /tmp/hironx-robot-script-$PID.tgz $USERID@$HOSTNAME:/tmp/
+    scp /tmp/hrpsys-base-${HRPSYS_VERSION}.zip /tmp/hironx-robot-script-$DATE.tgz $USERID@$HOSTNAME:/tmp/
     ssh $USERID@$HOSTNAME -t $commands 2>&1 | tee /tmp/robot-compile-hrpsys-`date +"%Y%m%d-%H%M%S"`.log
     echo "====="
-    echo "$ cd /opt/jsk"
-    echo "$ tar -xvzf /tmp/hrpsys-qnx-${HRPSYS_VERSION}.tgz"
+    echo "$ tar -xvzf /tmp/hrpsys-${HRPSYS_VERSION}-qnx-${DATE}; cd hrpsys-source-$DATE/build; make install"
 else
     echo "DO NOT RUN"
     echo "----"
--- hironx_ros_bridge/robot/robot-compile-openhrp3.sh
+++ hironx_ros_bridge/robot/robot-compile-openhrp3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function usage {
-    echo >&2 "usage: $0 [hostname (default:hiro014)]"
+    echo >&2 "usage: $0 [hostname (default:hiro014)] [username (default:hiro)]"
     echo >&2 "          [-h|--help] print this message"
     exit 0
 }
@@ -22,11 +22,8 @@ while [ -n "$1" ] ; do
     esac
 done
 
-PID=28169
-
-wget https://raw.github.com/start-jsk/openhrp3/master/package.xml -O /tmp/openhrp3-package.xml
-OPENHRP3_VERSION=`sed -n 's@\s*<version>\(.*\)</version>@\1@p' /tmp/openhrp3-package.xml`
-
+DATE=`date +%Y-%m-%d`
+OPENHRP3_VERSION=3.1.5
 
 commands="
   . ~/.profile;
@@ -34,20 +31,20 @@ commands="
   trap 'exit 1' ERR;
   set +x;
   echo \"* Download openhrp3 *\";
-  mkdir -p /tmp/openhrp3-source-$PID/src;
-  cd /tmp/openhrp3-source-$PID/src;
-  (echo 'p' | svn co https://openrtp.jp/svn/hrg/openhrp/3.1/tags/${OPENHRP3_VERSION} OpenHRP-${OPENHRP3_VERSION}) ||  tar -xzf /tmp/OpenHRP-${OPENHRP3_VERSION}.tgz;
+  mkdir -p /tmp/openhrp3-source-$DATE/src;
+  cd /tmp/openhrp3-source-$DATE/src;
+  (echo 'p' | svn co --trust-server-cert --non-interactive https://openrtp.jp/svn/hrg/openhrp/3.1/tags/${OPENHRP3_VERSION} OpenHRP-${OPENHRP3_VERSION}) ||  tar -xzf /tmp/OpenHRP-${OPENHRP3_VERSION}.tgz;
   echo \"* Configure openhrp3 *\";
-  cd OpenHRP-${OPENHRP3_VERSION};
-  PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig CXX=QCC CC=qcc LDFLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem\" cmake . -DEIGEN_INCLUDE_DIR=/opt/jsk/include/eigen3 -DLAPACK_LIBRARY_DIRS=/opt/jsk/lib -DLAPACK_INCLUDE_DIRS=/opt/jsk/include -DOPENRTM_DIR=/opt/jsk -DENABLE_INSTALL_RPATH=ON -DENABLE_INSTALL_RPATH_TO_SELF=ON -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex lf2c -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=../../;
+  mkdir -p ../build;
+  cd ../build;
+  PKG_CONFIG_PATH=/opt/jsk/lib/pkgconfig:/usr/pkg/lib/pkgconfig:/usr/local/lib/pkgconfig CXX=QCC CC=qcc LDFLAGS=\"-L/usr/local/lib -L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -latlas -lblas\" cmake ../src/OpenHRP-${OPENHRP3_VERSION} -DEIGEN_INCLUDE_DIR=/opt/jsk/include/eigen3 -DLAPACK_LIBRARY_DIRS=/usr/local/lib -DLAPACK_INCLUDE_DIRS=/usr/local/atlas/include -DOPENRTM_DIR=/opt/jsk -DENABLE_INSTALL_RPATH=ON -DENABLE_INSTALL_RPATH_TO_SELF=ON -DCOMPILE_JAVA_STUFF=OFF -DCMAKE_SHARED_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -latlas -lblas -luuid -lsocket -Wl,-u,MAIN__\" -DCMAKE_EXE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -luuid -lsocket -Wl,-u,MAIN__\" -DCMAKE_MODULE_LINKER_FLAGS=\"-L/usr/pkg/lib -lboost_system -lboost_signals -lboost_filesystem -lboost_regex -lf2c -latlas -lblas -Wl,-u,MAIN__\" -DCMAKE_INSTALL_PREFIX=/opt/jsk;
   echo \"* Compile openhrp3 *\";
   PATH=/opt/jsk/bin:/usr/pkg/bin:/usr/qnx650/host/qnx6/x86/usr/bin:$PATH LD_LIBRARY_PATH=/opt/jsk/lib:/usr/pkg/lib:/usr/qnx650/host/qnx6/x86/usr/lib make VERBOSE=1;
+  echo \"* install openhrp3 *\";
   make install;
-  echo \"* Modify codes *\";
-  cd /tmp;
-  (cd openhrp3-source-$PID; sed -i s@/tmp/openhrp3-source-$PID@/opt/jsk@g lib/pkgconfig/openhrp3.1.pc bin/openhrp-jython-prompt include/OpenHRP-3.1/hrpModel/Config.h;);
   echo \"* make tarball*\";
-  tar -C openhrp3-source-$PID  -czf openhrp3-qnx-${OPENHRP3_VERSION}.tgz ./;
+  cd /tmp;
+  tar -czf openhrp3-$OPENHRP3_VERSION-qnx-$DATE.tgz ./openhrp3-source-$DATE/;
   "
 
 hostname=$1
@@ -59,8 +56,7 @@ read -p "execute compile command @ $hostname (y/n)?"
 if [ "$REPLY" == "y" ]; then
     ssh $userid@$hostname -t $commands 2>&1 | tee /tmp/robot-compile-openhrp3-`date +"%Y%m%d-%H%M%S"`.log
     echo "====="
-    echo "$ cd /opt/jsk"
-    echo "$ tar -xvzf /tmp/openhrp3-qnx-${OPENHRP3_VERSION}.tgz"
+    echo "$ tar -xvzf /tmp/openhrp3-${OPENHRP3_VERSION}-qnx-${DATE}.tgz; cd openhrp3-${OPENHRP3_VERSION}-qnx-$DATE/build; make install"
 else
     echo "DO NOT RUN"
     echo "----"
