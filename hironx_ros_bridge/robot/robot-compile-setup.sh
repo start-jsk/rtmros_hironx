8260c66b673d75be157c810b69be675b4fd62d75
lokada <lokada@jenkins.jsk.imi.i.u-tokyo.ac.jp>
2014-07-06 18:11:48 +0900

    add robot-compile-setup.sh

---
+++ hironx_ros_bridge/robot/robot-compile-setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function usage {
+    echo >&2 "usage: $0 [hostname (default:hiro014)] [username (default:hiro)]"
+    echo >&2 "          [-h|--help] print this message"
+    exit 0
+}
+
+# command line parse
+OPT=`getopt -o h -l help -- $*`
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- $OPT
+
+while [ -n "$1" ] ; do
+    case $1 in
+        -h|--help) usage ;;
+        --) shift; break;;
+        *) echo "Unknown option($1)"; usage;;
+    esac
+done
+
+hostname=$1
+hostname=${hostname:="hiro014"} 
+userid=$2
+userid=${userid:="hiro"} 
+
+DATE=`date +%Y-%m-%d`
+
+TMPDIR=/tmp/HOGE
+mkdir -p ${TMPDIR}/opt/jsk
+wget https://github.com/tork-a/rtmros_nextage/archive/0.2.14.zip -O ${TMPDIR}/rtmros_nextage-0.2.14.zip || echo "ERROR:: Failed to download source code"
+wget 'https://docs.google.com/uc?authuser=0&id=0B5hXrFUpyR2iZS0tQlFyXzhjaGc&export=download' -O ${TMPDIR}/opt-jsk-base.tgz || echo "ERROR:: Failed to download source code"
+
+(cd ${TMPDIR}; tar -xvzf opt-jsk-base.tgz)
+(cd ${TMPDIR}; unzip -o rtmros_nextage-0.2.14.zip)
+mkdir -p ${TMPDIR}/opt/jsk/etc
+mv ${TMPDIR}/rtmros_nextage-0.2.14/nextage_description/ ${TMPDIR}/opt/jsk/etc/
+tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk/
+
+commands="
+  . ~/.profile;
+  env;
+  trap 'exit 1' ERR;
+  set +x;
+  echo \"* setup /opt/jsk directory *\";
+  cd /;
+  tar -xvzf /tmp/opt-jsk-base-model.tgz;
+  "
+
+echo "comands = $commands"
+read -p "execute compile command @ $hostname (y/n)? "
+if [ "$REPLY" == "y" ]; then
+    scp ${TMPDIR}/opt-jsk-base-model.tgz $userid@$hostname:/tmp/
+    ssh $userid@$hostname -t $commands 2>&1 | tee /tmp/robot-compile-setup-`date +"%Y%m%d-%H%M%S"`.log
+    echo "====="
+else
+    echo "DO NOT RUN"
+    echo "----"
+    echo "$commands"
+    echo "----"
+    echo "EXITTING.."
+fi
+
+rm -fr ${TMPDIR}
