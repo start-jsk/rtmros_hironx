083d14953155eebfd5f35bd42b5eca3c82f9af47
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-12-15 11:42:50 +0900

    Fix default frame name

--- hironx_ros_bridge/src/hironx_ros_bridge/hironx_client.py
+++ hironx_ros_bridge/src/hironx_ros_bridge/hironx_client.py
@@ -672,3 +672,31 @@ class HIRONX(HrpsysConfigurator):
 
     def removeForceSensorOffset(self):
         self.rh_svc.removeForceSensorOffset()
+
+    def getCurrentPose(self, lname=None, frame_name='WAIST'):
+        HrpsysConfigurator.getCurrentPose(self, lname, frame_name)
+    
+    def getCurrentPosition(self, lname=None, frame_name='WAIST'):
+        HrpsysConfigurator.getCurrentPosition(self, lname, frame_name)
+
+    def getCurrentRotation(self, lname=None, frame_name='WAIST'):
+        HrpsysConfigurator.getCurrentRotation(self, lname, frame_name)
+    
+    def getCurrentRPY(self, lname, frame_name='WAIST'):
+        HrpsysConfigurator.getCurrentRPY(self, lname, frame_name)
+
+    def getReferencePose(self, lname, frame_name='WAIST'):
+        HrpsysConfigurator.getReferencePose(self, lname, frame_name)
+
+    def getReferencePosition(self, lname, frame_name='WAIST'):
+        HrpsysConfigurator.getReferencePosition(self, lname, frame_name)
+
+    def getReferenceRotation(self, lname, frame_name='WAIST'):
+        HrpsysConfigurator.getReferenceRotation(self, lname, frame_name)
+
+    def getReferenceRPY(self, lname, frame_name='WAIST'):
+        HrpsysConfigurator.getReferenceRPY(self, lname, frame_name)
+
+    def setTargetPose(self, gname, pos, rpy, tm, frame_name='WAIST'):
+        HrpsysConfigurator.setTargetPose(self, gname, pos, rpy, tm, frame_name)
+
