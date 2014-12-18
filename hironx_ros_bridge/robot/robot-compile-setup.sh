7e53a66b140f31bcd36dd451fd045eec56adc1ba
Isaac IY Saito <130s@lateeye.net>
2014-12-18 22:06:16 +0900

    (robot install) Let hrpsys versioning happen under /opt/jsk, not /opt. https://github.com/tork-a/delivery/issues/33#issuecomment-67120688

--- hironx_ros_bridge/robot/robot-compile-setup.sh
+++ hironx_ros_bridge/robot/robot-compile-setup.sh
@@ -34,7 +34,7 @@ DATE=`date +%Y-%m-%d`
 TMPDIR=/tmp/HiroNXO_install
 URL_HIRONX_ZIPBALL=https://github.com/start-jsk/rtmros_hironx/archive/1.0.27.zip
 URL_NXO_ZIPBALL_STABLE=https://github.com/tork-a/rtmros_nextage/archive/0.2.14.zip
-mkdir -p ${TMPDIR}/opt/jsk_${hrpsys_version}
+mkdir -p ${TMPDIR}/opt/jsk/${hrpsys_version}
 # Download model files.
 wget ${URL_HIRONX_ZIPBALL} -O ${TMPDIR}/rtmros_hironx.zip || echo "ERROR:: Failed to download hironx zipball."
 wget ${URL_NXO_ZIPBALL_STABLE} -O ${TMPDIR}/rtmros_nextage.zip || echo "ERROR:: Failed to download nxo zipball."
@@ -45,14 +45,14 @@ wget 'https://docs.google.com/uc?authuser=0&id=0B5hXrFUpyR2iZS0tQlFyXzhjaGc&expo
 (cd ${TMPDIR}; unzip -o rtmros_hironx.zip)
 (cd ${TMPDIR}; unzip -o rtmros_nextage.zip)
 # The tarball above (opt-jsk-base.tgz) contains `include` and `lib` folders, only. So from here we'll create other folders.
-mkdir -p ${TMPDIR}/opt/jsk_${hrpsys_version}/etc
-mv ${TMPDIR}/*/hironx_ros_bridge/ ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/HIRONX
+mkdir -p ${TMPDIR}/opt/jsk/${hrpsys_version}/etc
+mv ${TMPDIR}/*/hironx_ros_bridge/ ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/HIRONX
 # zipball retrieved by URL_NXO_ZIPBALL_STABLE yields random folder name. So skip it by asterisk.
-mv ${TMPDIR}/*/nextage_description/ ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/NEXTAGE
+mv ${TMPDIR}/*/nextage_description/ ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE
 # Folder names from nextage_description and the one used internal to QNX is different. See https://github.com/start-jsk/rtmros_hironx/issues/160#issuecomment-48572336
-mv ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/HIRONX/models ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/HIRONX/model
-mv ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/NEXTAGE/models ${TMPDIR}/opt/jsk_${hrpsys_version}/etc/NEXTAGE/model
-tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk_${hrpsys_version}/
+mv ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/HIRONX/models ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/HIRONX/model
+mv ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE/models ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE/model
+tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk/${hrpsys_version}/
 
 # Command that gets run on QNX. 
 # Assumption: NO /opt/jsk directory exists on QNX.
@@ -65,14 +65,15 @@ commands="
   echo \"* If /opt/jsk is a symlink, remove it (it'll be generated later within this script. *\";
   echo \"* Else if /opt/jsk is a directory, not a symlink, it's a folder containing OSS controller libraries. *\";
   echo \"* So move it aside with hrpsys version and the timestamp as a part of dir name (eg. /opt/jsk_315.2.0_20141214). *\";
-  test -L /opt/jsk && su -c 'rm -fr /opt/jsk' || su -c 'mv /opt/jsk /opt/jsk_$(strings RobotHardware.so | grep ^[0-9]*\\.[0-9]*\\.[0-9]*$)_$(date +%Y%m%d)'
-  echo \"* setup /opt/jsk_$hrpsys_version directory *\";
+  test -L /opt/jsk && su -c 'rm -fr /opt/jsk' || su -c 'mv /opt/jsk /opt/jsk/$(strings RobotHardware.so | grep ^[0-9]*\\.[0-9]*\\.[0-9]*$)_$(date +%Y%m%d)'
+  echo \"* setup /opt/jsk/$hrpsys_version directory *\";
   cd /;
   su -c 'tar -xvzf /tmp/opt-jsk-base-model.tgz';
-  echo \"* Create supplemental folder '/opt/jsk_$hrpsys_version/var/log' *\";
-  su -c 'mkdir -p /opt/jsk_$hrpsys_version/var/log';
-  echo \"* Symbolic link from /opt/jsk to the one that contains specific hrpsys version. *\";
-  su -c 'ln -sf /opt/jsk_$hrpsys_version /opt/jsk';
+  echo \"* Create supplemental folder '/opt/jsk/$hrpsys_version/var/log' *\";
+  su -c 'mkdir -p /opt/jsk/$hrpsys_version/var/log';
+  echo \"* Symbolic link from folders in /opt/jsk to the ones that contain specific hrpsys version. *\";
+  cd /opt/jsk
+  su -c 'ln -sf /opt/jsk/$hrpsys_version/* .';
   "
 
 echo "Necessary files are prepared on your host and now ready to run these commands inside the robot = $commands"
