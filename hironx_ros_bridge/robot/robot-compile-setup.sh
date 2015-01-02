2828a532365e781378705b2ed48f412374890eeb
Isaac IY Saito <130s@lateeye.net>
2015-01-01 17:07:40 -0800

    (robot qnx install) Capitalize variables.

--- hironx_ros_bridge/robot/robot-compile-setup.sh
+++ hironx_ros_bridge/robot/robot-compile-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function usage {
-    echo >&2 "usage: $0 [hostname (default:hiro014)] [username (default:hiro)] [hrpsys_version (default:315.2.8)]"
+    echo >&2 "usage: $0 [HOSTNAME_QNX (default:hiro014)] [USERNAME_QNX (default:hiro)] [HRPSYS_VER (default:315.2.8)]"
     echo >&2 "          [-h|--help] print this message"
     exit 0
 }
@@ -22,22 +22,22 @@ while [ -n "$1" ] ; do
     esac
 done
 
-hostname=$1
-hostname=${hostname:="hiro014"} 
-userid=$2
-userid=${userid:="hiro"} 
-hrpsys_version=$3
-hrpsys_version=${hrpsys_version:="315.2.8"} 
+HOSTNAME_QNX=$1
+HOSTNAME_QNX=${HOSTNAME_QNX:="hiro014"} 
+USERNAME_QNX=$2
+USERNAME_QNX=${USERNAME_QNX:="hiro"} 
+HRPSYS_VER=$3
+HRPSYS_VER=${HRPSYS_VER:="315.2.8"} 
 
 DATE=`date +%Y-%m-%d`
 
 # Detect if the target robot is Hiro or NX.
-#ssh $userid@$hostname ls /opt/hiro && export IS_TARGET_HIRO=true
+#ssh $USERNAME_QNX@$HOSTNAME_QNX ls /opt/hiro && export IS_TARGET_HIRO=true
 
 TMPDIR=/tmp/HiroNXO_install
 URL_HIRONX_ZIPBALL=https://github.com/start-jsk/rtmros_hironx/archive/1.0.27.zip
 URL_NXO_ZIPBALL_STABLE=https://github.com/tork-a/rtmros_nextage/archive/0.2.14.zip
-mkdir -p ${TMPDIR}/opt/jsk/${hrpsys_version}
+mkdir -p ${TMPDIR}/opt/jsk/${HRPSYS_VER}
 # Download model files.
 ##wget ${URL_HIRONX_ZIPBALL} -O ${TMPDIR}/rtmros_hironx.zip || echo "ERROR:: Failed to download hironx zipball."
 ##(cd ${TMPDIR}; unzip -o rtmros_hironx.zip)
@@ -48,20 +48,20 @@ wget 'https://docs.google.com/uc?authuser=0&id=0B5hXrFUpyR2iZS0tQlFyXzhjaGc&expo
 
 (cd ${TMPDIR}; tar -xvzf opt-jsk-base.tgz)
 # The tarball above (opt-jsk-base.tgz) contains `include` and `lib` folders, only. So from here we'll create other folders.
-mkdir -p ${TMPDIR}/opt/jsk/${hrpsys_version}/etc
-##mv ${TMPDIR}/*/hironx_ros_bridge/ ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/HIRONX
+mkdir -p ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc
+##mv ${TMPDIR}/*/hironx_ros_bridge/ ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/HIRONX
 # zipball retrieved by URL_NXO_ZIPBALL_STABLE yields random folder name. So skip it by asterisk.
-mv ${TMPDIR}/*/nextage_description/ ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE
+mv ${TMPDIR}/*/nextage_description/ ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE
 # VRML model file of Hiro is not opensourced yet. It should be found in /opt/jsk/etc/HIRONX in each robot.
 # If Hiro, fetch tarball of /opt/hiro to the /tmp/hiro_wrl.tgz on the local machine, then extract the content.
-ssh $userid@$hostname ls /opt/hiro && (export IS_TARGET_HIRO="true"; ssh $userid@$hostname "tar cfvz /tmp/hiro_wrl.tgz /opt/jsk/etc/HIRONX;"; scp $userid@$hostname:/tmp/hiro_wrl.tgz /tmp/hiro_wrl.tgz; cd ${TMPDIR} && tar xfvz /tmp/hiro_wrl.tgz ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/; ls ${TMPDIR}) || echo "The robot you're dealing with now is NEXTAGE."
+ssh $USERNAME_QNX@$HOSTNAME_QNX ls /opt/hiro && (export IS_TARGET_HIRO="true"; ssh $USERNAME_QNX@$HOSTNAME_QNX "tar cfvz /tmp/hiro_wrl.tgz /opt/jsk/etc/HIRONX;"; scp $USERNAME_QNX@$HOSTNAME_QNX:/tmp/hiro_wrl.tgz /tmp/hiro_wrl.tgz; cd ${TMPDIR} && tar xfvz /tmp/hiro_wrl.tgz ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/; ls ${TMPDIR}) || echo "The robot you're dealing with now is NEXTAGE."
 # Folder names from nextage_description and the one used internal to QNX is different. See https://github.com/start-jsk/rtmros_hironx/issues/160#issuecomment-48572336
-mv ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE/models ${TMPDIR}/opt/jsk/${hrpsys_version}/etc/NEXTAGE/model
+mv ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE/models ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE/model
 # Create a tarball that contains model files of both Hiro and NXO.
-tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk/${hrpsys_version}/
+tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk/${HRPSYS_VER}/
 
 # Get the version of hrpsys that is working inside of QNX.
-scp $userid@$hostname:/opt/jsk/lib/RobotHardware.so /tmp
+scp $USERNAME_QNX@$HOSTNAME_QNX:/opt/jsk/lib/RobotHardware.so /tmp
 HRPSYS_PREV_INSTALLED=$(strings /tmp/RobotHardware.so  | grep ^[0-9]*\\.[0-9]*\\.[0-9]*$)
 
 # Command that gets run on QNX. 
@@ -76,21 +76,21 @@ commands="
   echo \"* Else if /opt/jsk is a directory, not a symlink, it's a folder containing OSS controller libraries. *\";
   echo \"* So move it aside with hrpsys version and the timestamp as a part of dir name (eg. /opt/jsk/315.2.0_20141214). *\";
   test -L /opt/jsk && su -c 'rm -fr /opt/jsk' || (mkdir /tmp/$HRPSYS_PREV_INSTALLED && su -c 'mv /opt/jsk/* /tmp/$HRPSYS_PREV_INSTALLED'; echo 'ls /opt/jsk' && ls /opt/jsk && su -c 'mv /tmp/$HRPSYS_PREV_INSTALLED /opt/jsk');
-  echo \"* setup /opt/jsk/$hrpsys_version directory *\";
+  echo \"* setup /opt/jsk/$HRPSYS_VER directory *\";
   cd /;
   su -c 'tar -xvzf /tmp/opt-jsk-base-model.tgz';
-  echo \"* Create supplemental folder '/opt/jsk/$hrpsys_version/var/log' *\";
-  su -c 'mkdir -p /opt/jsk/$hrpsys_version/var/log';
+  echo \"* Create supplemental folder '/opt/jsk/$HRPSYS_VER/var/log' *\";
+  su -c 'mkdir -p /opt/jsk/$HRPSYS_VER/var/log';
   echo \"* Symbolic link from folders in /opt/jsk to the ones that contain specific hrpsys version. Sym-linking didn't work from the directory. *\";
   cd /opt/jsk;
-  su -c 'ln -sf /opt/jsk/$hrpsys_version/* .';
+  su -c 'ln -sf /opt/jsk/$HRPSYS_VER/* .';
   "
 
 echo "Necessary files are prepared on your host and now ready to run these commands inside the robot = $commands"
-read -p "execute compile command @ $hostname (y/n)? "
+read -p "execute compile command @ $HOSTNAME_QNX (y/n)? "
 if [ "$REPLY" == "y" ]; then
-    scp ${TMPDIR}/opt-jsk-base-model.tgz $userid@$hostname:/tmp/
-    ssh $userid@$hostname -t $commands 2>&1 | tee /tmp/robot-compile-setup-`date +"%Y%m%d-%H%M%S"`.log
+    scp ${TMPDIR}/opt-jsk-base-model.tgz $USERNAME_QNX@$HOSTNAME_QNX:/tmp/
+    ssh $USERNAME_QNX@$HOSTNAME_QNX -t $commands 2>&1 | tee /tmp/robot-compile-setup-`date +"%Y%m%d-%H%M%S"`.log
     echo "====="
 else
     echo "DO NOT RUN"
