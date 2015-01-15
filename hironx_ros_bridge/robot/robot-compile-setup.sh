f6f1a6738c9f5d9069b67686c269bf338bab75c3
Isaac IY Saito <130s@lateeye.net>
2015-01-15 15:33:42 -0800

    (robot install) Fetch model files from QNX if the robot is Hiro.

--- hironx_ros_bridge/robot/robot-compile-setup.sh
+++ hironx_ros_bridge/robot/robot-compile-setup.sh
@@ -31,10 +31,9 @@ HRPSYS_VER=${HRPSYS_VER:="315.2.8"}
 
 DATE=`date +%Y-%m-%d`
 
-# Detect if the target robot is Hiro or NX.
-#ssh $USERNAME_QNX@$HOSTNAME_QNX ls /opt/hiro && export IS_TARGET_HIRO=true
-
 TMPDIR=/tmp/HiroNXO_install
+NAME_LOGFILE=${TMPDIR}/robot-compile-setup-`date +"%Y%m%d-%H%M%S"`.log
+
 URL_HIRONX_ZIPBALL=https://github.com/start-jsk/rtmros_hironx/archive/1.0.27.zip
 URL_NXO_ZIPBALL_STABLE=https://github.com/tork-a/rtmros_nextage/archive/0.2.14.zip
 mkdir -p ${TMPDIR}/opt/jsk/${HRPSYS_VER}
@@ -45,24 +44,37 @@ wget ${URL_NXO_ZIPBALL_STABLE} -O ${TMPDIR}/rtmros_nextage.zip || echo "ERROR:: 
 (cd ${TMPDIR}; unzip -o rtmros_nextage.zip)
 # Download 'base' libraries for QNX that don't be affected by the hrpsys version
 wget 'https://docs.google.com/uc?authuser=0&id=0B5hXrFUpyR2iZS0tQlFyXzhjaGc&export=download' -O ${TMPDIR}/opt-jsk-base.tgz || echo "ERROR:: Failed to download opt-jsk-base.tgz."
+# Move files in the tarball above to ${HRPSYS_VER}
+(cd ${TMPDIR}; tar -xvzf opt-jsk-base.tgz; mv ./opt/jsk/include ${TMPDIR}/opt/jsk/${HRPSYS_VER}; mv ./opt/jsk/lib ${TMPDIR}/opt/jsk/${HRPSYS_VER})
 
-(cd ${TMPDIR}; tar -xvzf opt-jsk-base.tgz)
 # The tarball above (opt-jsk-base.tgz) contains `include` and `lib` folders, only. So from here we'll create other folders.
 mkdir -p ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc
 ##mv ${TMPDIR}/*/hironx_ros_bridge/ ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/HIRONX
 # zipball retrieved by URL_NXO_ZIPBALL_STABLE yields random folder name. So skip it by asterisk.
 mv ${TMPDIR}/*/nextage_description/ ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE
 # VRML model file of Hiro is not opensourced yet. It should be found in /opt/jsk/etc/HIRONX in each robot.
-# If Hiro, fetch tarball of /opt/hiro to the /tmp/hiro_wrl.tgz on the local machine, then extract the content.
-ssh $USERNAME_QNX@$HOSTNAME_QNX ls /opt/hiro && (export IS_TARGET_HIRO="true"; ssh $USERNAME_QNX@$HOSTNAME_QNX "tar cfvz /tmp/hiro_wrl.tgz /opt/jsk/etc/HIRONX;"; scp $USERNAME_QNX@$HOSTNAME_QNX:/tmp/hiro_wrl.tgz /tmp/hiro_wrl.tgz; cd ${TMPDIR} && tar xfvz /tmp/hiro_wrl.tgz ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/; ls ${TMPDIR}) || echo "The robot you're dealing with now is NEXTAGE."
+# Detect if the target robot is Hiro or NX.
+# If Hiro, fetch tarball of /opt/hiro to the ${TMPDIR}/hiro_wrl.tgz on the local machine, then extract the content.
+IS_QNX_HIRO=is_qnx_hiro
+commands_check_hiro="
+  ls /opt/hiro && (echo 'Ok, the robot on QNX is Hiro.' && tar cfvz $TMPDIR/hiro_wrl.tgz /opt/jsk/etc/HIRONX && true) || (echo 'No, this robot on QNX is NXO.'; false)" && (echo 'true' > $TMPDIR/$IS_QNX_HIRO);
+  "
+(ssh $USERNAME_QNX@$HOSTNAME_QNX -t $commands_check_hiro; echo '----$commands_check_hiro is over.----') 2>&1 | tee -a ${NAME_LOGFILE}
+#    Then get the tarball from Hiro.
+if [ -e $TMPDIR/$IS_QNX_HIRO ];
+then
+  (scp $USERNAME_QNX@$HOSTNAME_QNX:${TMPDIR}/hiro_wrl.tgz ${TMPDIR}/hiro_wrl.tgz; echo 'Create a tarball for hiro model files.' && cd ${TMPDIR} && tar xfvz ${TMPDIR}/hiro_wrl.tgz ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/) && (echo 'Model files of Hiro are retrieved from robot.' && ls ${TMPDIR} || echo "The robot you're dealing with now is NEXTAGE."
+else
+  echo 'Nothing done for NXO here.'  
+fi
 # Folder names from nextage_description and the one used internal to QNX is different. See https://github.com/start-jsk/rtmros_hironx/issues/160#issuecomment-48572336
 mv ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE/models ${TMPDIR}/opt/jsk/${HRPSYS_VER}/etc/NEXTAGE/model
 # Create a tarball that contains model files of both Hiro and NXO.
 tar -C ${TMPDIR} -cvzf ${TMPDIR}/opt-jsk-base-model.tgz ./opt/jsk/${HRPSYS_VER}/
 
 # Get the version of hrpsys that is working inside of QNX.
-scp $USERNAME_QNX@$HOSTNAME_QNX:/opt/jsk/lib/RobotHardware.so /tmp
-HRPSYS_PREV_INSTALLED=$(strings /tmp/RobotHardware.so  | grep ^[0-9]*\\.[0-9]*\\.[0-9]*$)
+scp $USERNAME_QNX@$HOSTNAME_QNX:/opt/jsk/lib/RobotHardware.so ${TMPDIR}
+HRPSYS_PREV_INSTALLED=$(strings ${TMPDIR}/RobotHardware.so | grep ^[0-9]*\\.[0-9]*\\.[0-9]*$)
 
 # Command that gets run on QNX. 
 # Assumption: NO /opt/jsk directory exists on QNX.
@@ -75,10 +87,10 @@ commands="
   echo \"* If /opt/jsk is a symlink, remove it (it'll be generated later within this script. *\";
   echo \"* Else if /opt/jsk is a directory, not a symlink, it's a folder containing OSS controller libraries. *\";
   echo \"* So move it aside with hrpsys version and the timestamp as a part of dir name (eg. /opt/jsk/315.2.0_20141214). *\";
-  test -L /opt/jsk && su -c 'rm -fr /opt/jsk' || (mkdir /tmp/$HRPSYS_PREV_INSTALLED && su -c 'mv /opt/jsk/* /tmp/$HRPSYS_PREV_INSTALLED'; echo 'ls /opt/jsk' && ls /opt/jsk && su -c 'mv /tmp/$HRPSYS_PREV_INSTALLED /opt/jsk');
+  test -L /opt/jsk && su -c 'rm -fr /opt/jsk' || (mkdir ${TMPDIR}/$HRPSYS_PREV_INSTALLED && su -c 'mv /opt/jsk/* ${TMPDIR}/$HRPSYS_PREV_INSTALLED'; echo 'ls /opt/jsk' && ls /opt/jsk && su -c 'mv ${TMPDIR}/$HRPSYS_PREV_INSTALLED /opt/jsk');
   echo \"* setup /opt/jsk/$HRPSYS_VER directory *\";
   cd /;
-  su -c 'tar -xvzf /tmp/opt-jsk-base-model.tgz';
+  su -c 'tar -xvzf ${TMPDIR}/opt-jsk-base-model.tgz';
   echo \"* Create supplemental folder '/opt/jsk/$HRPSYS_VER/var/log' *\";
   su -c 'mkdir -p /opt/jsk/$HRPSYS_VER/var/log';
   echo \"* Symbolic link from folders in /opt/jsk to the ones that contain specific hrpsys version. Sym-linking didn't work from the directory. *\";
@@ -87,10 +99,11 @@ commands="
   "
 
 echo "Necessary files are prepared on your host and now ready to run these commands inside the robot = $commands"
-read -p "execute compile command @ $HOSTNAME_QNX (y/n)? "
+read -p "execute installation command @ $HOSTNAME_QNX (y/n)? "
 if [ "$REPLY" == "y" ]; then
-    scp ${TMPDIR}/opt-jsk-base-model.tgz $USERNAME_QNX@$HOSTNAME_QNX:/tmp/
-    ssh $USERNAME_QNX@$HOSTNAME_QNX -t $commands 2>&1 | tee /tmp/robot-compile-setup-`date +"%Y%m%d-%H%M%S"`.log
+    echo "scp ${TMPDIR}/opt-jsk-base-model.tgz into $HOSTNAME_QNX"
+    scp ${TMPDIR}/opt-jsk-base-model.tgz $USERNAME_QNX@$HOSTNAME_QNX:${TMPDIR}
+    ssh $USERNAME_QNX@$HOSTNAME_QNX -t $commands 2>&1 | tee -a ${NAME_LOGFILE} 
     echo "====="
 else
     echo "DO NOT RUN"
