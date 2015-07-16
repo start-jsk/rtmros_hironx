39b5ff457ba472ec78c0dea2fd8fbda0572d83d4
Kei Okada <kei.okada@gmail.com>
2015-07-16 17:16:08 +0900

    [setup.py] add to install hrpsys_315_1_9

--- hironx_ros_bridge/setup.py
+++ hironx_ros_bridge/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['hironx_ros_bridge', 'hironx_ros_bridge.testutil'],
+    packages=['hironx_ros_bridge', 'hironx_ros_bridge.testutil', 'hironx_ros_bridge.hrpsys_315_1_9'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
