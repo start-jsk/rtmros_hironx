2377742d79185f563dd97ac9313e941ec389ea4c
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-05-02 03:24:45 -0700

    [hironx_clinet.py] Auto-complete impedance parameters.
    Parameters in `startImpedance` method is very important for the safety, as the robot's behavior can become unexpectable with inappropriate values. In my experience the "good" parameters change per runtime, and can only be figured by carefully observing the F/T sensor's output, and it's the user's responsibility to fill in the parameters.
    
    Clearly all of these processes makes the method call error-prone.
    This change intends to automatically fill the impedance parameters, in particular reference force "`ref_force`".
    
    One concern is that this change makes `HIRONX` class and `hironx_client.py` module dependent on ROS, although the entire class/module have been ROS-agnostic. This is trivial and ok IMO, since there are other files in `hironx_ros_bridge` package that depends on ROS (i.e. ROS is everywhere).

--- hironx_ros_bridge/test/test_hironx_controller.py
+++ hironx_ros_bridge/test/test_hironx_controller.py
@@ -33,19 +33,21 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from distutils.version import StrictVersion
+
 from test_hironx import *
 
 
 class TestHiroController(TestHiro):
 
     # https://github.com/fkanehiro/hrpsys-base/blob/master/sample/SampleRobot/samplerobot_impedance_controller.py.in
-    def test_impedance_controller(self):  # https://github.com/start-jsk/rtmros_hironx/issues/337
+    def test_impedance_controller(self, **kwargs):  # https://github.com/start-jsk/rtmros_hironx/issues/337
         if not self.robot.ic or self.robot.ic.ref.get_component_profile().version < '315.3.0':
             self.assertTrue(True)
             return True
         # although this is not stable rtc, we'll test them
         self.robot.goInitial(tm=1)
-        ret = self.robot.startImpedance('rarm')  # this returns ret, this is bug
+        ret = self.robot.startImpedance('rarm', **kwargs)  # this returns ret, this is bug
         # self.assertTrue(ret)
         ret = self.robot.seq_svc.setWrenches([0, 0, 0, 0, 0, 0,
                                               10, 0, 0, 0, 0, 0, ], 2.0)
@@ -71,6 +73,41 @@ class TestHiroController(TestHiro):
         ret = self.robot.stopImpedance('rarm')
         # self.assertTrue(ret)
         self.assertTrue(True)  # this is dummy, current simulate hiro does not have force sensor so it retunrs None
+
+    def test_impedance_controller_refforce_315_1(self):
+        '''
+        @summary: Pass ref_force for hrpsys < 315.2.0.
+                  Although Impedance Controller is not a stable RTC,
+                  we'll test it.
+        '''
+        KIN_GROUP = 'rarm'  # Target kinematic group.
+        _REFFORCE_DEFAULT = [0.5, 0.5, 0.5]
+        _REFFORCE_2 = [0.5, 0.5, 0.5]
+        if not self.robot.ic or StrictVersion('315.2.0') <= StrictVersion(self.robot.hrpsys_version):
+            self.assertTrue(True)
+            return True
+
+        self.robot.goInitial(tm=1)
+        self.robot.startImpedance(KIN_GROUP)
+        # ic_svc.getImpedanceControllerParam returns something like:
+        #
+        # (True, OpenHRP.ImpedanceControllerService.impedanceParam(
+        #            M_p=100.0, D_p=200.0, K_p=400.0, M_r=2000.0, D_r=100.0, K_r=200.0, 
+        #            force_gain=[1.0, 1.0, 1.0], moment_gain=[0.0, 0.0, 0.0],
+        #            sr_gain=1.0, avoid_gain=0.0, reference_gain=0.0,
+        #            manipulability_limit=0.1, controller_mode=MODE_IMP,
+        #            ik_optional_weight_vector=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        #            use_sh_base_pos_rpy=False))
+        self.assertListEqual(
+            _REFFORCE_DEFAULT,
+            self.robot.ic_svc.getImpedanceControllerParam(KIN_GROUP).ref_force)
+
+        # Test startImpedance with ref_force as an arg.
+        self.robot.startImpedance(KIN_GROUP, ref_force=_REFFORCE_2)
+        self.assertListEqual(
+            _REFFORCE_2,
+            self.robot.ic_svc.getImpedanceControllerParam(KIN_GROUP).ref_force)
+        self.robot.stopImpedance(KIN_GROUP)
 
     def test_hands_controller(self):
         '''
