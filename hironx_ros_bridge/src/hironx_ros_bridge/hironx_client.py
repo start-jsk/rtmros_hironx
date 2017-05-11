2975d456af02f4dc07fe326470eb560d8d35beaf
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-05-11 13:12:35 -0700

    [ros_bridge.py][startImpedance_315_3] More precise error message.
    When run by itself, `getImpedanceControllerParam` command at least finishes, so it's not failing. The error message is a bit misleading about what happened internally in `startImpedance_315_3` method.

--- hironx_ros_bridge/src/hironx_ros_bridge/hironx_client.py
+++ hironx_ros_bridge/src/hironx_ros_bridge/hironx_client.py
@@ -1000,7 +1000,7 @@ class HIRONX(HrpsysConfigurator2):
         '''
         r, p = self.ic_svc.getImpedanceControllerParam(arm)
         if not r:
-            print('{}, Failt to getImpedanceControllerParam({})'.format(self.configurator_name, arm))
+            print('{}, impedance parameter not found for {}.'.format(self.configurator_name, arm))
             return False
         if M_p != None: p.M_p = M_p
         if D_p != None: p.M_p = D_p
