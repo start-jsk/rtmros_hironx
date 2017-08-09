8a3e58678eee3e3e1f13b3cde0c9819c8d7e43d3
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2017-08-09 23:22:37 +0000

    fix error function, display tet_results

--- .travis.sh
+++ .travis.sh
@@ -4,8 +4,9 @@ set -x
 
 function error {
   if [ $BUILDER == rosbuild ]; then find ${HOME}/.ros/rosmake/ -type f -exec echo "=== {} ===" \; -exec cat {} \; ; fi
-  if [ ${HOME}/.ros/test_results ]; then find ${HOME}/.ros/test_results -type f -exec echo "=== {} ===" \; -exec cat {} \; ; fi
-  for file in ${HOME}/.ros/log/rostest-*; do echo "=== $file ==="; cat $file; done
+  if [ ~/ros/ws_$REPOSITORY_NAME/build/test_results ]; then find ~/ros/ws_$REPOSITORY_NAME/build/test_results -type f -exec echo "=== {} ===" \; -exec cat {} \; ; fi
+  #if [ ${HOME}/.ros/test_results ]; then find ${HOME}/.ros/test_results -type f -exec echo "=== {} ===" \; -exec cat {} \; ; fi
+  #for file in ${HOME}/.ros/log/rostest-*; do echo "=== $file ==="; cat $file; done
   exit 1
 }
 
