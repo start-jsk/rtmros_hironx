f8bdee425437ff59d331a054b314791bfd2d7fc4
Shun Hasegawa <pazeshun3684@gmail.com>
2018-11-22 15:39:53 +0900

    Add version to idl dir name

--- hironx_ros_bridge/cmake/rtmbuild_customed.cmake
+++ hironx_ros_bridge/cmake/rtmbuild_customed.cmake
@@ -13,15 +13,170 @@ endif()
 
 
 ##
-## See rtmbuild/cmake/rtmbuild.cmake for GLOBAL VARIABLES
+## GLOBAL VARIABLES
 ##
+## openrtm_aist_INCLUDE_DIRS
+## openrtm_aist_LIBRARIES
+## openhrp3_INCLUDE_DIRS
+## openhrp3_LIBRARIES
+## idl2srv_EXECUTABLE
+## rtmskel_EXECUTABLE
+## ${PROJECT_NAME}_idl_files
+## ${PROJECT_NAME}_autogen_files
+## ${PROJECT_NAME}_autogen_msg_files
+## ${PROJECT_NAME}_autogen_srv_files
+## ${PROJECT_NAME}_autogen_interfaces
+## rtm_idlc, rtm_idlflags, rtm_idldir
+## rtm_cxx,  rtm_cflags
+## hrp_idldir
+
+
+# Based on _rtmbuild_get_idls in rtmbuild/cmake/servicebridge.cmake
+# Change points from original _rtmbuild_get_idls:
+# - get idl dir name as argument
+# get idl dir name as argument
+# macro(_rtmbuild_get_idls)
+macro(_rtmbuild_get_idls_from_dir _idl_dir)
+  # get idl dir name as argument
+  # file(GLOB _idl_files "${PROJECT_SOURCE_DIR}/idl/*.idl")  ## get full path
+  file(GLOB _idl_files "${PROJECT_SOURCE_DIR}/${_idl_dir}/*.idl")  ## get full path
+  foreach(_idl_file ${_idl_files})
+    # copy from rosbuild_get_msgs to avoid .#Foo.idl, by emacs
+    if(${_idl_file} MATCHES "/[^\\.]+\\.idl$")
+      list(APPEND ${PROJECT_NAME}_idl_files ${_idl_file})
+    endif()
+  endforeach(_idl_file)
+endmacro(_rtmbuild_get_idls_from_dir)
+
+#
+# setup global variables
+#
+# Change points from original rtmbuild_init:
+# - get idl dir name as argument
+# - use idl2srv.py in hironx_ros_bridge
+# - use _rtmbuild_get_idls_from_dir instead of _rtmbuild_get_idls
+# get idl dir name as argument
+# macro(rtmbuild_init)
+macro(rtmbuild_init_from_dir _idl_dir)
+  # get idl dir name as argument
+  # set(_extra_message_dependencies ${ARGV0})
+  set(_extra_message_dependencies ${ARGV1})
+  if(NOT use_catkin) ## rosbuild_init cleans all variable so we first defind project and call rosbuild_init later with ROSBUILD_DONT_REDEFINE_PROJECT=TRUE
+    get_filename_component(_project ${CMAKE_SOURCE_DIR} NAME)
+    project(${_project})
+
+    rosbuild_find_ros_package(openrtm_aist)
+    rosbuild_find_ros_package(openhrp3)
+    set(ENV{PKG_CONFIG_PATH} ${openrtm_aist_PACKAGE_PATH}/lib/pkgconfig:${openhrp3_PACKAGE_PATH}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH})
+    message("[rtmbuild_init] - ENV{PKG_CONFIG_PATH} -  > $ENV{PKG_CONFIG_PATH}")
+  endif()
+  #
+  # use pkg-config to set --cflags --libs plus rtm-related flags
+  #
+  find_package(PkgConfig)
+  pkg_check_modules(openrtm_aist openrtm-aist REQUIRED)
+  pkg_check_modules(openhrp3 openhrp3.1)
+  message("[rtmbuild_init] Building package ${CMAKE_SOURCE_DIR} ${PROJECT_NAME}")
+  message("[rtmbuild_init] - CATKIN_TOPLEVEL = ${CATKIN_TOPLEVEL}")
+  if(DEBUG_RTMBUILD_CMAKE)
+    message("[rtmbuild_init] - openrtm_aist_INCLUDE_DIRS -> ${openrtm_aist_INCLUDE_DIRS}")
+    message("[rtmbuild_init] - openrtm_aist_LIBRARIES    -> ${openrtm_aist_LIBRARIES}")
+    message("[rtmbuild_init] - openhrp3_INCLUDE_DIRS -> ${openhrp3_INCLUDE_DIRS}")
+    message("[rtmbuild_init] - openhrp3_LIBRARIES    -> ${openhrp3_LIBRARIES}")
+  endif()
+
+  # use idl2srv.py in hironx_ros_bridge
+  # if(EXISTS ${rtmbuild_SOURCE_PREFIX}) # catkin
+  #   set(idl2srv_EXECUTABLE ${rtmbuild_SOURCE_PREFIX}/scripts/idl2srv.py)
+  # elseif(EXISTS ${rtmbuild_PACKAGE_PATH}) ## for rosbuild
+  #   set(idl2srv_EXECUTABLE ${rtmbuild_PACKAGE_PATH}/scripts/idl2srv.py)
+  # else()
+  #   pkg_check_modules(rtmbuild rtmbuild REQUIRED)
+  #   set(idl2srv_EXECUTABLE ${rtmbuild_PREFIX}/share/rtmbuild/scripts/idl2srv.py)
+  # endif()
+  set(idl2srv_EXECUTABLE ${PROJECT_SOURCE_DIR}/scripts/idl2srv.py)
+  message("[rtmbuild_init] - idl2srv_EXECUTABLE     -> ${idl2srv_EXECUTABLE}")
+
+  execute_process(COMMAND pkg-config openrtm-aist --variable=prefix      OUTPUT_VARIABLE rtm_prefix    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(EXISTS ${rtm_prefix}/bin/rtm-skelwrapper)
+    set(_rtm_exe_path ${rtm_prefix}/bin)
+  else()
+    set(_rtm_exe_path ${rtm_prefix}/lib/openrtm_aist/bin)
+  endif()
+  set(rtmskel_EXECUTABLE PATH=${_rtm_exe_path}:$ENV{PATH} PYTHONPATH=${openrtm_aist_PREFIX}/lib/openrtm-1.1/py_helper:$ENV{PYTHONPATH} ${_rtm_exe_path}/rtm-skelwrapper)
+  message("[rtmbuild_init] - rtmskel_EXECUTABLE     -> ${rtmskel_EXECUTABLE}")
+
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_idlc     OUTPUT_VARIABLE rtm_idlc     OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_idlflags OUTPUT_VARIABLE rtm_idlflags OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(rtm_idlflags "${rtm_idlflags} -Wbuse_quotes") # IDLs in hrpsys-base needs this option because of https://github.com/start-jsk/rtmros_common/issues/861. We can remove this after openrtm-aist.pc is updated.
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_idldir   OUTPUT_VARIABLE rtm_idldir   OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_cxx      OUTPUT_VARIABLE rtm_cxx      OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_cflags   OUTPUT_VARIABLE rtm_cflags   OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND pkg-config openrtm-aist --variable=rtm_libs     OUTPUT_VARIABLE rtm_libs     OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND pkg-config openhrp3.1   --variable=idl_dir      OUTPUT_VARIABLE hrp_idldir   OUTPUT_STRIP_TRAILING_WHITESPACE)
+  separate_arguments(rtm_idlflags)
+  separate_arguments(rtm_cflags)
+  separate_arguments(rtm_libs)
+  set(rtm_cxx "c++") ## openrtm-aist --variable=rtm_cxx sometimes returns /usr/lib/ccache/c++
+  message("[rtmbuild_init] - rtm_idlc               -> ${rtm_idlc}")
+  message("[rtmbuild_init] - rtm_idlflags           -> ${rtm_idlflags}")
+  message("[rtmbuild_init] - rtm_idldir             -> ${rtm_idldir}")
+  message("[rtmbuild_init] - rtm_cxx                -> ${rtm_cxx}")
+  message("[rtmbuild_init] - rtm_cflags             -> ${rtm_cflags}")
+  message("[rtmbuild_init] - rtm_libs               -> ${rtm_libs}")
+  message("[rtmbuild_init] - hrp_idldir             -> ${hrp_idldir}")
+
+  ##
+  ## get idl files and store to _idl_list
+  message("[rtmbuild_init] Generating bridge compornents from ${PROJECT_SOURCE_DIR}/idl")
+  set(${PROJECT_NAME}_idl_files "")
+  # use _rtmbuild_get_idls_from_dir instead of _rtmbuild_get_idls
+  # _rtmbuild_get_idls() ## set ${PROJECT_NAME}_idl_files
+  _rtmbuild_get_idls_from_dir(${_idl_dir}) ## set ${PROJECT_NAME}_idl_files
+  message("[rtmbuild_init] - ${PROJECT_NAME}_idl_files : ${${PROJECT_NAME}_idl_files}")
+  if(NOT ${PROJECT_NAME}_idl_files)
+    message(AUTHOR_WARNING "[rtmbuild_init] - no idl file is defined")
+  endif()
+
+  ## generate msg/srv/cpp from idl
+  set(${PROJECT_NAME}_autogen_msg_files "")
+  set(${PROJECT_NAME}_autogen_srv_files "")
+  _rtmbuild_genbridge_init()
+  message("[rtmbuild_init] - ${PROJECT_NAME}_autogen_msg_files  : ${${PROJECT_NAME}_autogen_msg_files}")
+  message("[rtmbuild_init] - ${PROJECT_NAME}_autogen_srv_files  : ${${PROJECT_NAME}_autogen_srv_files}")
+  message("[rtmbuild_init] - ${PROJECT_NAME}_autogen_interfaces : ${${PROJECT_NAME}_autogen_interfaces}")
+  set(rtmbuild_${PROJECT_NAME}_autogen_msg_files ${${PROJECT_NAME}_autogen_msg_files}) 
+
+  ##
+  ## rosbulid_init for rosbuild
+  if(NOT use_catkin)
+    set(ROSBUILD_DONT_REDEFINE_PROJECT TRUE)
+    rosbuild_init()
+  endif()
+
+  if(use_catkin)
+    add_message_files(DIRECTORY msg FILES "${${PROJECT_NAME}_autogen_msg_files}")
+    add_service_files(DIRECTORY srv FILES "${${PROJECT_NAME}_autogen_srv_files}")
+    generate_messages(DEPENDENCIES std_msgs ${_extra_message_dependencies})
+  else()
+    rosbuild_genmsg()
+    rosbuild_gensrv()
+  endif()
+
+  include_directories(${catkin_INCLUDE_DIRS} ${openrtm_aist_INCLUDE_DIRS} ${openhrp3_INCLUDE_DIRS})
+  link_directories(${catkin_LIBRARY_DIRS} ${openrtm_aist_LIBRARY_DIRS} ${openhrp3_LIBRARY_DIRS})
+
+endmacro(rtmbuild_init_from_dir)
 
 # add_custom_command to compile idl/*.idl file into c++
 # Change points from original rtmbuild_genidl:
+# - use idl dir name given as argument
 # - don't generate python
 # - don't overwrite lib of CORBA skeleton and stub from other pkgs to avoid compile error in those pkgs
-macro(rtmbuild_genidl_customed)
-  message("[rtmbuild_genidl_customed] add_custom_command for idl files in package ${PROJECT_NAME}")
+# use idl dir name given as argument
+# macro(rtmbuild_genidl)
+macro(rtmbuild_genidl_from_dir _idl_dir)
+  message("[rtmbuild_genidl_from_dir] add_custom_command for idl files in package ${PROJECT_NAME}")
 
   set(_autogen "")
 
@@ -34,66 +189,89 @@ macro(rtmbuild_genidl_customed)
     # set(_output_python_dir ${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_PYTHON_DESTINATION}/${PROJECT_NAME})
     unset(_output_python_dir)
   else()
-    set(_output_dir ${PROJECT_SOURCE_DIR}/idl_gen)
-    set(_output_cpp_dir ${PROJECT_SOURCE_DIR}/idl_gen/cpp/${PROJECT_NAME})
-    set(_output_lib_dir ${PROJECT_SOURCE_DIR}/idl_gen/lib)
+    # use idl dir name given as argument
+    # set(_output_dir ${PROJECT_SOURCE_DIR}/idl_gen)
+    # set(_output_cpp_dir ${PROJECT_SOURCE_DIR}/idl_gen/cpp/${PROJECT_NAME})
+    # set(_output_lib_dir ${PROJECT_SOURCE_DIR}/idl_gen/lib)
+    set(_output_dir ${PROJECT_SOURCE_DIR}/${_idl_dir}_gen)
+    set(_output_cpp_dir ${PROJECT_SOURCE_DIR}/${_idl_dir}_gen/cpp/${PROJECT_NAME})
+    set(_output_lib_dir ${PROJECT_SOURCE_DIR}/${_idl_dir}_gen/lib)
     # don't generate python
     # set(_output_python_dir ${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME})
     unset(_output_python_dir)
-    include_directories(${PROJECT_SOURCE_DIR}/idl_gen/cpp/)
+    # use idl dir name given as argument
+    # include_directories(${PROJECT_SOURCE_DIR}/idl_gen/cpp/)
+    include_directories(${PROJECT_SOURCE_DIR}/${_idl_dir}_gen/cpp/)
   endif()
 
   # don't generate python
   # set(_output_idl_py_files "")
   unset(_output_idl_py_files)
   set(_output_idl_hh_files "")
-  file(MAKE_DIRECTORY ${_output_cpp_dir}/idl)
+  # use idl dir name given as argument
+  # file(MAKE_DIRECTORY ${_output_cpp_dir}/idl)
+  file(MAKE_DIRECTORY ${_output_cpp_dir}/${_idl_dir})
   file(MAKE_DIRECTORY ${_output_lib_dir})
   link_directories(${_output_lib_dir})
 
-  message("[rtmbuild_genidl_customed] - _output_cpp_dir : ${_output_cpp_dir}")
-  message("[rtmbuild_genidl_customed] - _output_lib_dir : ${_output_lib_dir}")
+  message("[rtmbuild_genidl_from_dir] - _output_cpp_dir : ${_output_cpp_dir}")
+  message("[rtmbuild_genidl_from_dir] - _output_lib_dir : ${_output_lib_dir}")
   # don't generate python
   # message("[rtmbuild_genidl] - _output_python_dir : ${_output_python_dir}")
 
   ## RTMBUILD_${PROJECT_NAME}_genrpc) depends on each RTMBUILD_${PROJECT_NAME}_${_idl_name}_genrpc)
   add_custom_target(RTMBUILD_${PROJECT_NAME}_genrpc)
   if(NOT ${PROJECT_NAME}_idl_files)
-    message(AUTHOR_WARNING "[rtmbuild_genidl_customed] - no idl file is defined")
+    message(AUTHOR_WARNING "[rtmbuild_genidl_from_dir] - no idl file is defined")
   endif()
   foreach(_idl_file ${${PROJECT_NAME}_idl_files})
     get_filename_component(_idl_name ${_idl_file} NAME_WE)
-    message("[rtmbuild_genidl_customed] - _idl_file : ${_idl_file}")
-    message("[rtmbuild_genidl_customed] - _idl_name : ${_idl_name}")
+    message("[rtmbuild_genidl_from_dir] - _idl_file : ${_idl_file}")
+    message("[rtmbuild_genidl_from_dir] - _idl_name : ${_idl_name}")
 
-    # set(_input_idl ${PROJECT_SOURCE_DIR}/idl/${_idl})
+    ## use idl dir name given as argument
+    ## set(_input_idl ${PROJECT_SOURCE_DIR}/idl/${_idl})
+    # set(_input_idl ${PROJECT_SOURCE_DIR}/${_idl_dir}/${_idl})
 
-    set(_output_idl_hh ${_output_cpp_dir}/idl/${_idl_name}.hh)
+    # use idl dir name given as argument
+    # set(_output_idl_hh ${_output_cpp_dir}/idl/${_idl_name}.hh)
+    set(_output_idl_hh ${_output_cpp_dir}/${_idl_dir}/${_idl_name}.hh)
     # don't generate python
     # set(_output_idl_py ${_output_python_dir}/${_idl_name}_idl.py)
     unset(_output_idl_py)
-    set(_output_stub_h ${_output_cpp_dir}/idl/${_idl_name}Stub.h)
-    set(_output_skel_h ${_output_cpp_dir}/idl/${_idl_name}Skel.h)
-    set(_output_stub_cpp ${_output_cpp_dir}/idl/${_idl_name}Stub.cpp)
-    set(_output_skel_cpp ${_output_cpp_dir}/idl/${_idl_name}Skel.cpp)
+    # use idl dir name given as argument
+    # set(_output_stub_h ${_output_cpp_dir}/idl/${_idl_name}Stub.h)
+    # set(_output_skel_h ${_output_cpp_dir}/idl/${_idl_name}Skel.h)
+    # set(_output_stub_cpp ${_output_cpp_dir}/idl/${_idl_name}Stub.cpp)
+    # set(_output_skel_cpp ${_output_cpp_dir}/idl/${_idl_name}Skel.cpp)
+    set(_output_stub_h ${_output_cpp_dir}/${_idl_dir}/${_idl_name}Stub.h)
+    set(_output_skel_h ${_output_cpp_dir}/${_idl_dir}/${_idl_name}Skel.h)
+    set(_output_stub_cpp ${_output_cpp_dir}/${_idl_dir}/${_idl_name}Stub.cpp)
+    set(_output_skel_cpp ${_output_cpp_dir}/${_idl_dir}/${_idl_name}Skel.cpp)
     set(_output_stub_lib ${_output_lib_dir}/lib${_idl_name}Stub.so)
     set(_output_skel_lib ${_output_lib_dir}/lib${_idl_name}Skel.so)
     list(APPEND ${PROJECT_NAME}_IDLLIBRARY_DIRS lib${_idl_name}Stub.so lib${_idl_name}Skel.so)
     # call the  rule to compile idl
     if(DEBUG_RTMBUILD_CMAKE)
-      message("[rtmbuild_genidl_customed] ${_output_idl_hh}\n -> ${_idl_file} ${${_idl}_depends}")
-      message("[rtmbuild_genidl_customed] ${_output_stub_cpp} ${_output_skel_cpp} ${_output_stub_h} ${_output_skel_h}\n -> ${_output_idl_hh}")
-      message("[rtmbuild_genidl_customed] ${_output_stub_lib} ${_output_skel_lib}\n -> ${_output_stub_cpp} ${_output_stub_h} ${_output_skel_cpp} ${_output_skel_h}")
+      message("[rtmbuild_genidl_from_dir] ${_output_idl_hh}\n -> ${_idl_file} ${${_idl}_depends}")
+      message("[rtmbuild_genidl_from_dir] ${_output_stub_cpp} ${_output_skel_cpp} ${_output_stub_h} ${_output_skel_h}\n -> ${_output_idl_hh}")
+      message("[rtmbuild_genidl_from_dir] ${_output_stub_lib} ${_output_skel_lib}\n -> ${_output_stub_cpp} ${_output_stub_h} ${_output_skel_cpp} ${_output_skel_h}")
     endif()
     # cpp
     add_custom_command(OUTPUT ${_output_idl_hh}
-      COMMAND ${rtm_idlc} ${rtm_idlflags} -C${_output_cpp_dir}/idl ${_idl_file}
+      # use idl dir name given as argument
+      # COMMAND ${rtm_idlc} ${rtm_idlflags} -C${_output_cpp_dir}/idl ${_idl_file}
+      COMMAND ${rtm_idlc} ${rtm_idlflags} -C${_output_cpp_dir}/${_idl_dir} ${_idl_file}
       DEPENDS ${_idl_file})
     add_custom_command(OUTPUT ${_output_stub_cpp} ${_output_skel_cpp} ${_output_stub_h} ${_output_skel_h}
-      COMMAND cp ${_idl_file} ${_output_cpp_dir}/idl
+      # use idl dir name given as argument
+      # COMMAND cp ${_idl_file} ${_output_cpp_dir}/idl
+      COMMAND cp ${_idl_file} ${_output_cpp_dir}/${_idl_dir}
       COMMAND rm -f ${_output_stub_cpp} ${_output_skel_cpp} ${_output_stub_h} ${_output_skel_h}
       COMMAND ${rtmskel_EXECUTABLE} --include-dir="" --skel-suffix=Skel --stub-suffix=Stub  --idl-file=${_idl_file}
-      WORKING_DIRECTORY ${_output_cpp_dir}/idl
+      # use idl dir name given as argument
+      # WORKING_DIRECTORY ${_output_cpp_dir}/idl
+      WORKING_DIRECTORY ${_output_cpp_dir}/${_idl_dir}
       DEPENDS ${_output_idl_hh})
     add_custom_command(OUTPUT ${_output_stub_lib} ${_output_skel_lib}
       COMMAND ${rtm_cxx} ${rtm_cflags} -I. -shared -o ${_output_stub_lib} ${_output_stub_cpp} ${rtm_libs}
@@ -139,11 +317,11 @@ macro(rtmbuild_genidl_customed)
 
   if(_autogen)
     if(DEBUG_RTMBUILD_CMAKE)
-      message("[rtmbuild_genidl_customed] ADDITIONAL_MAKE_CLEAN_FILES : ${_autogen}")
+      message("[rtmbuild_genidl_from_dir] ADDITIONAL_MAKE_CLEAN_FILES : ${_autogen}")
     endif()
     # Also set up to clean the srv_gen directory
     get_directory_property(_old_clean_files ADDITIONAL_MAKE_CLEAN_FILES)
     list(APPEND _old_clean_files ${_autogen})
     set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${_old_clean_files}")
   endif(_autogen)
-endmacro(rtmbuild_genidl_customed)
+endmacro(rtmbuild_genidl_from_dir)
